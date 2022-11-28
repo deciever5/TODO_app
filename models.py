51d44e5be33890ca569d661d3673d0ef41dc3f50
deciever5 <marcinkunikowski@gmail.com>
2022-11-28 10:12:38 +0100

    base for library app done

--- models.py
+++ models.py
@@ -16,17 +16,29 @@ class Todos:
         return self.todos[id]
 
     def create(self, data):
-        data.pop('csrf_token')
         self.todos.append(data)
+        self.save_all()
 
     def save_all(self):
         with open("todos.json", "w") as f:
             json.dump(self.todos, f)
 
     def update(self, id, data):
-        data.pop('csrf_token')
-        self.todos[id] = data
-        self.save_all()
+        todo = self.get(id)
+        if todo:
+            index = self.todos.index(todo)
+            self.todos[index] = data
+            self.save_all()
+            return True
+        return False
+
+    def delete(self, id):
+        todo = self.get(id)
+        if todo:
+            self.todos.remove(todo)
+            self.save_all()
+            return True
+        return False
 
 
 todos = Todos()
